c40ef1901ac169856f60377ca5265eaa42d69574
63brown <mutheemumbi01@gmail.com>
2023-05-11 17:25:55 +0300

    Top ten posts

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Module allows you to send HTTP requests 
+using Python"""
+import requests
+
+def top_ten(subreddit):
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {'User-Agent': 'Mozilla /5.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code != 200:
+        print(None)
+        return
+
+    data = response.json()
+    posts = data['data']['children']
+
+    for post in posts[:10]:
+        title = post['data']['title']
+        print(title)
