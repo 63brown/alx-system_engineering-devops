7da69360d671fb00bbf3519d8cfbf5080782a8e4
63brown <mutheemumbi01@gmail.com>
2023-05-11 15:02:24 +0300

    How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
-"""Module allows you to send HTTP requests using Python"""
+"""Module allows you to send HTTP requests 
+using Python"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """a function that queries the Reddit API and returns the number of subscribers"""
+    """
+    A function that queries the Reddit API 
+    and returns the number of subscribers"""
+    
     headers = {'User-Agent': 'Mozilla/5.0'}
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
     response = requests.get(url, headers=headers, allow_redirects=False)
