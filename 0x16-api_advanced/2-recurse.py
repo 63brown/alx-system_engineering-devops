474703c4613d7021d8c3860e27d943fdde0ed701
63brown <mutheemumbi01@gmail.com>
2023-05-11 18:26:18 +0300

    a sorted count of given keywords

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -4,6 +4,9 @@ import requests
 
 
 def recurse(subreddit, hot_list=[], after=None):
+    """
+    recursive function that queries the Reddit API and returns a list containing the titles of all hot articles for a given subreddit
+    """
     url = "https://www.reddit.com/r/{}/hot.json?after={}".format(subreddit, after) if after else "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {'User-Agent': 'Mozila/5.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
