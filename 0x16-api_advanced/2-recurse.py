d44b1c4076beecf7b4aff949aac9f12386ba042c
63brown <mutheemumbi01@gmail.com>
2023-05-11 18:44:22 +0300

    list containing the titles of all hot articles

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -5,19 +5,29 @@ import requests
 
 def recurse(subreddit, hot_list=[], after=None):
     """
-    recursive function that queries the Reddit API and returns a list containing the titles of all hot articles for a given subreddit
+    Recursive function that queries the Reddit API and returns a list
+    containing the titles of all hot articles for a given subreddit.
     """
-    url = "https://www.reddit.com/r/{}/hot.json?after={}".format(subreddit, after) if after else "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {'User-Agent': 'Mozila/5.0'}
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    if after:
+        url += "?after={}".format(after)
+
+    headers = {'User-Agent': 'Mozilla/5.0'}
+
     response = requests.get(url, headers=headers, allow_redirects=False)
+
     if response.status_code != 200:
         return None
+
     data = response.json()
     posts = data['data']['children']
+
     for post in posts:
         title = post['data']['title']
         hot_list.append(title)
+
     after = data['data']['after']
     if after:
-        recurse(subreddit, hot_list, after=after)
+        return recurse(subreddit, hot_list, after=after)
+
     return hot_list
