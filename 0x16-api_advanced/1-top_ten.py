194e0d23db6375b36bfb20f37b75c0111aba2a4d
63brown <mutheemumbi01@gmail.com>
2023-05-11 17:32:41 +0300

    Top Ten Posts

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-"""Module allows you to send HTTP requests 
-using Python"""
+"""Module allows you to send HTTP requests using Python"""
 import requests
 
 def top_ten(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    headers = {'User-Agent': 'Mozilla /5.0'}
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code != 200:
