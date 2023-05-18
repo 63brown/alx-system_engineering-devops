3c615e9a80cba87c44ef940f35c735168db1af45
63brown <mutheemumbi01@gmail.com>
2023-05-18 17:27:28 +0300

    debugging

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,10 @@
+#Change OS configuration
+exec { 'set soft limit to 2000':
+  path    => '/bin',
+  command => "sed -i 's/4/2000/' /etc/security/limits.conf"
+}
+
+exec { 'set hard limit to 4000':
+  path    => '/bin',
+  command => "sed -i 's/5/4000/' /etc/security/limits.conf"
+}
